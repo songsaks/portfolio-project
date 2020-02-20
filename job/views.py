4a8c962f69b1c4036b5a303e573ea29699be8ef1
songsak <aeekae@gmail.com>
2020-02-20 10:02:38 +0700

    bootstarp is showing up

--- job/views.py
+++ job/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Job
 # Create your views here.
+def home(request):
+    jobs = Job.objects #get all of the Job objects and store in jobs variable
+    return render(request, 'jobs/home.html', { 'jobs1':jobs })
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'job.apps.JobConfig',
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -113,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Bangkok'
 
 USE_I18N = True
 

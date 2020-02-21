5a737ae78696f47bbf6b036534d46cac87bbd9ad
songsak <aeekae@gmail.com>
2020-02-21 16:15:31 +0700

    end of static sections

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
-# Creabte your models here.
+# Creabte your 9models here.
 class Blog(models.Model):
     title = models.CharField(max_length=255)
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def sammary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
